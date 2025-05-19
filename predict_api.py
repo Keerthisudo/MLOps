9d9d7a16e8872d7a027dfaf244ab088d008cfacf
Keerthisudo <keerthisundarraj003@gmail.com>
2025-05-19 14:58:31 +0530

    Add Flask inference API for model predictions

---
+++ predict_api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+
+app = Flask(__name__)
+
+# Load data and train model
+X, y = load_iris(return_X_y=True)
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['features']  # Expecting a list of features
+    prediction = model.predict([data])
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(debug=True)
